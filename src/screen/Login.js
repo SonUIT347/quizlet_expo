fa72397ec8e3c23920f1011b38f13a640a1526b0
TaiTai3006 <101327980+TaiTai3006@users.noreply.github.com>
2023-02-21 18:08:38 +0700

    css sign in sign up

--- src/screen/Login.js
+++ src/screen/Login.js
@@ -1,106 +1,157 @@
-import { View, Text, SafeAreaView, TextInput, StyleSheet,TouchableOpacity } from 'react-native'
+import {
+  View,
+  Text,
+  SafeAreaView,
+  TextInput,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+} from "react-native";
+import { Button } from "@rneui/base";
 import React, { createContext, useState } from "react";
-import { Dimensions } from 'react-native'
-import {signInWithEmailAndPassword, onAuthStateChanged} from "firebase/auth";
-import Ionicons from "react-native-vector-icons/Ionicons"
-import { authentication } from '../firebase/firebaseConfig';
-import { useContext } from 'react';
-var widthfull = Dimensions.get('window').width; //full width
-var heightfull = Dimensions.get('window').height; //full height
+import { Dimensions } from "react-native";
+import { signInWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import { authentication } from "../firebase/firebaseConfig";
+import { useContext } from "react";
+var widthfull = Dimensions.get("window").width; //full width
+var heightfull = Dimensions.get("window").height; //full height
 
-const Login = ({navigation}) => {
-  const [userName, setUserName] = useState("")
-  const [Password, setPassword] = useState("")
-  const [eyeIcon, setEyeicon] = useState("eye")
-  const [typePass, setTypePass] = useState()
-  const [isSecure, setIsSecure] = useState(true)
-  const signIn = () =>{ 
+const Login = ({ navigation }) => {
+  const [userName, setUserName] = useState("");
+  const [Password, setPassword] = useState("");
+  const [eyeIcon, setEyeicon] = useState("eye");
+  const [typePass, setTypePass] = useState();
+  const [isSecure, setIsSecure] = useState(true);
+  const signIn = () => {
     signInWithEmailAndPassword(authentication, userName, Password)
-    .then((userCredential) => {
-      // Signed in 
-      const user = userCredential.user;
-      onAuthStateChanged(authentication, (user) => {
-        if (user) {
-          // User is signed in, see docs for a list of available properties
-          // https://firebase.google.com/docs/reference/js/firebase.User
-          const uid = user.uid;
-          // const userId = createContext(uid)
-          // console.log(userId.Provider)
-          navigation.navigate("TabBar"/*,{
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        onAuthStateChanged(authentication, (user) => {
+          if (user) {
+            // User is signed in, see docs for a list of available properties
+            // https://firebase.google.com/docs/reference/js/firebase.User
+            const uid = user.uid;
+            // const userId = createContext(uid)
+            // console.log(userId.Provider)
+            navigation.navigate(
+              "TabBar" /*,{
             userID: uid
-          }*/)
-          // console.log(uid)
-          // ...
-        } else {
-          // User is signed out
-          // ...
-        }
+          }*/
+            );
+            // console.log(uid)
+            // ...
+          } else {
+            // User is signed out
+            // ...
+          }
+        });
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        alert(errorCode);
       });
-      // ...
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      alert(errorCode)
-    });
-
-  }
+  };
   return (
-    <SafeAreaView style = {styles.container} >
-    <SafeAreaView style = {styles.textInput_ctn} >
-      <Text style={styles.text} >Email</Text>
-      <TextInput fontSize={15} onChangeText={(newtext) => setUserName(newtext) } style={styles.textInput} placeholder='email' placeholderTextColor={"gray"} >
-      </TextInput>
-      <Text style={styles.text} >Password</Text>
-      <TextInput fontSize={15} onChangeText={(newtext) => setPassword(newtext) } style={styles.textInput} secureTextEntry={isSecure} placeholder='Password' placeholderTextColor={"gray"} >
-      </TextInput>
-      {/* <TouchableOpacity onPress={() => setEyeicon("eye-off") } >
+    <SafeAreaView style={styles.container}>
+      <Image
+        style={{ width: "100%", height: "50%" }}
+        source={require("./Login.png")}
+      />
+      <SafeAreaView
+        style={[styles.textInput_ctn, { marginLeft: 30, marginRight: 30 }]}
+      >
+        <Text style={styles.text}>Email</Text>
+        <TextInput
+          fontSize={15}
+          onChangeText={(newtext) => setUserName(newtext)}
+          style={styles.textInput}
+          placeholder="Email"
+          placeholderTextColor={"gray"}
+        ></TextInput>
+        <View style={{ marginTop: 20 }}>
+          <Text style={styles.text}>Password</Text>
+          <TextInput
+            fontSize={15}
+            onChangeText={(newtext) => setPassword(newtext)}
+            style={styles.textInput}
+            secureTextEntry={isSecure}
+            placeholder="Password"
+            placeholderTextColor={"gray"}
+          ></TextInput>
+        </View>
+        {/* <TouchableOpacity onPress={() => setEyeicon("eye-off") } >
               <Ionicons
                         name={eyeIcon}
                         size={24}
                         color="white"
                       />
       </TouchableOpacity> */}
-      <TouchableOpacity style={{backgroundColor:"gray", marginTop:100, height:50,width:"60%", marginLeft:"20%", borderRadius:10}} onPress={() => (signIn())} >
-        <Text style={{textAlign:"center", marginTop:10, fontSize:20, color:"white"}} >Login</Text>
-
-    </TouchableOpacity>
-    <TouchableOpacity style={{backgroundColor:"gray", marginTop:10, height:50,width:"60%", marginLeft:"20%", borderRadius:10}} onPress={() => navigation.navigate("Register")} >
-        <Text style={{textAlign:"center", marginTop:10, fontSize:20, color:"white"}} >Register</Text>
-
-    </TouchableOpacity>
+        <Button
+          onPress={() => signIn()}
+          title="Đăng nhập"
+          buttonStyle={{
+            backgroundColor: "rgba(78, 116, 289, 1)",
+            borderRadius: 4,
+          }}
+          containerStyle={{
+            marginTop: 20,
+            marginLeft: 20,
+            marginRight: 20,
+          }}
+        />
+        <View style={{ flexDirection: "row" , marginTop: 20}}>
+          <Text style={{ color: "white", fontWeight: "600" }}>
+            Bạn đã đăng kí thành viên chưa?{" "}
+          </Text>
+          <TouchableOpacity onPress={() => navigation.navigate("Register")}>
+            <Text
+              style={{
+                color: "white",
+                fontWeight: "600",
+                color: "#FF8FA2",
+                textDecorationLine: "underline",
+              }}
+            >
+              Đăng ký ngay
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </SafeAreaView>
     </SafeAreaView>
-
-    </SafeAreaView>
-  )
-}
+  );
+};
 const styles = StyleSheet.create({
-  container:{
+  container: {
     width: widthfull,
-    height:heightfull,
-    backgroundColor:"#0A092D",
-    flexDirection:"column",
-    justifyContent:"center"
+    height: heightfull,
+    backgroundColor: "#0A092D",
+    flexDirection: "column",
+    justifyContent: "center",
   },
-  textInput_ctn:{
+  textInput_ctn: {
     //backgroundColor:"blue",
-    height:"50%",
+    height: "50%",
   },
-  textInput:{
-    alignSelf:"center",
-    borderBottomWidth:1,
-    width:"100%",
-    borderColor:"white",
-    backgroundColor:"white",
-    height:50
+  textInput: {
+    alignSelf: "center",
+    borderBottomWidth: 1,
+    width: "100%",
+    borderColor: "white",
+    backgroundColor: "white",
+    height: 50,
+    borderRadius: 4
   },
-  text:{
-    alignSelf:"flex-start",
-    fontSize:20,
-    color:"white"
+  text: {
+    alignSelf: "flex-start",
+    fontSize: 20,
+    color: "white",
   },
-  text_color:{
-    textColor:"white"
-  }
-})
-export default Login
+  text_color: {
+    textColor: "white",
+  },
+});
+export default Login;
--- src/screen/Register.js
+++ src/screen/Register.js
@@ -1,84 +1,118 @@
-import { View, Text, SafeAreaView, TextInput, StyleSheet,TouchableOpacity } from 'react-native'
+import {
+  View,
+  Text,
+  SafeAreaView,
+  TextInput,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+} from "react-native";
 import React, { useState } from "react";
-import { Dimensions } from 'react-native'
-import { authentication } from '../firebase/firebaseConfig';
-import { createUserWithEmailAndPassword } from 'firebase/auth';
-var widthfull = Dimensions.get('window').width; //full width
-var heightfull = Dimensions.get('window').height; //full height
+import { Button } from "@rneui/base";
+import { Dimensions } from "react-native";
+import { authentication } from "../firebase/firebaseConfig";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+var widthfull = Dimensions.get("window").width; //full width
+var heightfull = Dimensions.get("window").height; //full height
 
-const Register = ({navigation}) => {
-    const [userName, setUserName] = useState("")
-    const [Password, setPassword] = useState("")
-    const [eyeIcon, setEyeicon] = useState("eye")
-    const [typePass, setTypePass] = useState()
-    const [isSecure, setIsSecure] = useState(true)
-    const signUp = () =>{
-      createUserWithEmailAndPassword(authentication,userName, Password)
+const Register = ({ navigation }) => {
+  const [userName, setUserName] = useState("");
+  const [Password, setPassword] = useState("");
+  const [eyeIcon, setEyeicon] = useState("eye");
+  const [typePass, setTypePass] = useState();
+  const [isSecure, setIsSecure] = useState(true);
+  const signUp = () => {
+    createUserWithEmailAndPassword(authentication, userName, Password)
       .then((userCredential) => {
         const user = userCredential.user;
-        console.log("user")
-        navigation.navigate('Login')
+        console.log("user");
+        navigation.navigate("Login");
       })
       .catch((error) => {
-
-        alert(error)
-
+        alert(error);
       });
-    }
+  };
   return (
-    <SafeAreaView style = {styles.container} >
-    <SafeAreaView style = {styles.textInput_ctn} >
-      <Text style={styles.text} >User name</Text>
-      <TextInput fontSize={15} onChangeText={(newtext) => setUserName(newtext) } style={styles.textInput} placeholder='email' placeholderTextColor={"gray"} >
-      </TextInput>
-      <Text style={styles.text} >Password</Text>
-      <TextInput fontSize={15} onChangeText={(newtext) => setPassword(newtext) } style={styles.textInput} secureTextEntry={isSecure} placeholder='Password' placeholderTextColor={"gray"} >
-      </TextInput>
-      {/* <TouchableOpacity onPress={() => setEyeicon("eye-off") } >
+    <SafeAreaView style={styles.container}>
+      <Image
+        style={{ width: "100%", height: "50%" }}
+        source={require("./signup.png")}
+      />
+      <View style={{ marginLeft: 30, marginRight: 30 }}>
+        <SafeAreaView style={styles.textInput_ctn}>
+          <Text style={styles.text}>User name</Text>
+          <TextInput
+            fontSize={15}
+            onChangeText={(newtext) => setUserName(newtext)}
+            style={styles.textInput}
+            placeholder="Email"
+            placeholderTextColor={"gray"}
+          ></TextInput>
+          <View style={{ marginTop: 20 }}>
+            <Text style={styles.text}>Password</Text>
+            <TextInput
+              fontSize={15}
+              onChangeText={(newtext) => setPassword(newtext)}
+              style={styles.textInput}
+              secureTextEntry={isSecure}
+              placeholder="Password"
+              placeholderTextColor={"gray"}
+            ></TextInput>
+          </View>
+          {/* <TouchableOpacity onPress={() => setEyeicon("eye-off") } >
               <Ionicons
                         name={eyeIcon}
                         size={24}
                         color="white"
                       />
       </TouchableOpacity> */}
-      <TouchableOpacity style={{backgroundColor:"gray", marginTop:100, height:50,width:"60%", marginLeft:"20%", borderRadius:10}} onPress={() => (signUp())} >
-        <Text style={{textAlign:"center", marginTop:10, fontSize:20, color:"white"}} >Register</Text>
-
-    </TouchableOpacity>
+          <Button
+            onPress={() => signUp()}
+            title="Đăng ký"
+            buttonStyle={{
+              backgroundColor: "rgba(78, 116, 289, 1)",
+              borderRadius: 4,
+            }}
+            containerStyle={{
+              marginTop: 40,
+              marginLeft: 20,
+              marginRight: 20,
+            }}
+          />
+        </SafeAreaView>
+      </View>
     </SafeAreaView>
-
-    </SafeAreaView>
-  )
-
-}
+  );
+};
 const styles = StyleSheet.create({
-    container:{
-      width: widthfull,
-      height:heightfull,
-      backgroundColor:"#0A092D",
-      flexDirection:"column",
-      justifyContent:"center"
-    },
-    textInput_ctn:{
-      //backgroundColor:"blue",
-      height:"50%",
-    },
-    textInput:{
-      alignSelf:"center",
-      borderBottomWidth:1,
-      width:"100%",
-      borderColor:"white",
-      backgroundColor:"white",
-      height:50
-    },
-    text:{
-      alignSelf:"flex-start",
-      fontSize:20,
-      color:"white"
-    },
-    text_color:{
-      textColor:"white"
-    }
-  })
+  container: {
+    width: widthfull,
+    height: heightfull,
+    backgroundColor: "#0A092D",
+    flexDirection: "column",
+    justifyContent: "center",
+  },
+  textInput_ctn: {
+    //backgroundColor:"blue",
+    height: "50%",
+  },
+  textInput: {
+    alignSelf: "center",
+    borderBottomWidth: 1,
+    width: "100%",
+    borderColor: "white",
+    backgroundColor: "white",
+    height: 50,
+    borderRadius: 4,
+  },
+  text: {
+    alignSelf: "flex-start",
+    fontSize: 20,
+    color: "white",
+  },
+  text_color: {
+    textColor: "white",
+  },
+});
 
-export default Register
+export default Register;
